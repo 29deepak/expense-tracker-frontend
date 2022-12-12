284c805bb841ae13004b43985e94ce1012f393bd
deepak <deepak545608@gmail.com>
2022-12-12 16:41:03 +0530

    task 2

---
+++ login/login.js
@@ -0,0 +1,21 @@
+function login(e){
+    e.preventDefault()
+    const loginDetails={
+        email:e.target.email.value,
+        password:e.target.password.value
+
+    }
+    axios.post("http://localhost:4000/user/login",loginDetails)
+    .then(response=>{
+        if(response.status==200){
+            alert(response.data.message)
+        }else{
+            throw new Error(response.data.message)
+        }
+    })
+    .catch(err=>{
+        console.log(JSON.stringify(err))
+        document.body.innerHTML +=`<div style="color:red";>${err.message}</div>`
+    })
+    
+}
